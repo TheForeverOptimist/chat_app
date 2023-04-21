36a5b23f838a04bb779292eb89460e9d8c3eff0e
TheForeverOptimist <sean@elevate.app>
2023-04-21 10:13:10 -0400

    intialize chatapp

---
+++ src/ChatPage/ChatPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function ChatPage() {
+  return (
+    <div className="ChatPage">
+        <div className="chat-box">
+            <div className="wrapper">
+                {/* <form onSubmit={handleSubmit}></form> */}
+                <input type="text" />
+                <button type ="submit">Send</button>
+            </div>
+        </div>
+    <h1>Welcome to Better</h1>
+    </div>
+  )
+}
+
+export default ChatPage
